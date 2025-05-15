001ce8df67f88607089a9b253cb30333c00b997a
Altafalinadaf <altafalinadaf@gmail.com>
2025-05-15 13:03:31 +0530

    adding program

---
+++ object.js
@@ -0,0 +1,12 @@
+laptop = {
+    name:"hp laptop",
+    price : 60000,
+    brand:'HP brand'
+}
+console.log(laptop)
+console.log(laptop.name)
+console.log(laptop['name'])
+console.log(laptop.price)
+console.log(laptop.brand)
+
+
---
+++ switch.js
@@ -0,0 +1,14 @@
+num = 3
+switch (num){
+    case 0:
+        console.log("number is 0")
+        break;
+    case 3:
+        console.log("number is 3")
+        break;
+    case 5:
+        console.log("number is 5")
+        break;
+    default:
+        console.log("nothing ")
+}
