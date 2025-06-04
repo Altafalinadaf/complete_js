c8c59ce3714077fd84aad8ab88f53f9b701d9f91
Altafalinadaf <altafalinadaf@gmail.com>
2025-06-04 13:01:16 +0530

    adding some more programs

---
+++ condition.js
@@ -0,0 +1,11 @@
+array = [10,20,30,40,100,200,300,400]
+for( i=0;i<=6;i++){
+    console.log(array[i])
+}
+
+for(i in array){
+    console.log(array[i])
+}
+for (i of array){
+    console.log(i)
+}
---
+++ exception12.js
@@ -0,0 +1,12 @@
+
+console.log("hello world")
+
+array = [10,20,30,40,50]
+for(i=0;i<5;i++){
+    console.log(array[i])
+}
+try{
+    console.log(arry[1])
+}catch(err){
+    console.log("name is not corrrect")
+}
