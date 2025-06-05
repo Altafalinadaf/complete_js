d708aab51a43ab82f6bdb5a78ae0fef5b2ff493d
Altafalinadaf <altafalinadaf@gmail.com>
2025-06-05 12:45:12 +0530

    adding some more program

---
+++ loops3.js
@@ -0,0 +1,16 @@
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+
+for(i=0;i<=20;i=i+2){
+    console.log("Hi! Hello world "+i)
+}
+
+array = [10,20,30,40,50,60]
+console.log(array)
+for(i=0;i<=5;i++){
+    console.log(array[i])
+}
+
+
